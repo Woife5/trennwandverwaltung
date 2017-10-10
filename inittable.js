221b718e8a26b87d839a811b739a892d091ee8d3
Wolfgang Schwendtbauer <wolfgang.schwendtbauer@gmail.com>
2017-10-10 14:09:39 +0200

    Added inittable.js to initiate the table

---
+++ inittable.js
@@ -0,0 +1,18 @@
+var mysql = require('mysql');
+
+var con = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "rootpw",
+  database: "trennwand"
+});
+
+con.connect(function(err) {
+  if (err) throw err;
+  console.log("Connected!");
+  var sql = "create table trennwandtable(id int not null auto_increment,primary key(id),teachername varchar(100),`class` varchar(10),`day` varchar(10),`lesson` int, `duration` int, trennwand1 int,trennwand2 int, trennwand3 int, trennwand4 int);";
+  con.query(sql, function (err, result) {
+    if (err) throw err;
+    console.log("Table created");
+  });
+});
