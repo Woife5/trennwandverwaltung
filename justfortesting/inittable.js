6e257de3da6a9924f1a5f5bedddefd04bab8a3c6
Wolfgang Schwendtbauer <wolfgang.schwendtbauer@gmail.com>
2017-10-10 15:28:07 +0200

    Changed the "day" in the database to "date"

--- justfortesting/inittable.js
+++ justfortesting/inittable.js
@@ -10,7 +10,12 @@ var con = mysql.createConnection({
 con.connect(function(err) {
   if (err) throw err;
   console.log("Connected!");
-  var sql = "create table trennwandtable(id int not null auto_increment,primary key(id),teachername varchar(100),`class` varchar(10),`day` varchar(10),`lesson` int, `duration` int, trennwand1 int,trennwand2 int, trennwand3 int, trennwand4 int);";
+var sql = "drop table trennwandtable;";
+  con.query(sql, function (err, result) {
+    if (err) throw err;
+    console.log("Table dropped");
+  });
+  var sql = "create table trennwandtable(id int not null auto_increment,primary key(id),teachername varchar(100),`class` varchar(10),`date` varchar(10),`lesson` int, `duration` int, trennwand1 int,trennwand2 int, trennwand3 int, trennwand4 int);";
   con.query(sql, function (err, result) {
     if (err) throw err;
     console.log("Table created");
