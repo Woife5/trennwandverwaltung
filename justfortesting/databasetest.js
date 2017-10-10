adf4b655a25554bc25d6fb4bfffc4d798b9d1adf
Wolfgang Schwendtbauer <wolfgang.schwendtbauer@gmail.com>
2017-10-10 16:02:00 +0200

    Created a test for the database (insert and select)

---
+++ justfortesting/databasetest.js
@@ -0,0 +1,25 @@
+var path = require("path");
+var mysql = require('mysql');
+
+var con = mysql.createConnection({
+  host     : 'localhost',
+  user     : 'user',
+  password : 'userpw',
+  database : 'trennwand'
+});
+
+
+con.connect(function(err) {
+  if (err) throw err;
+  console.log("Connected!");
+  var sql = `"insert into trennwandtable values(null,"Matejka","5AHELS","23.10.2017",1,1,1,0,0,0),(null,"Matejka","4AHELS","24.10.2017",1,1,1,0,0,0),(null,"Wagner","3AHELS","23.10.2017",1,1,0,1,0,0),(null,"Matejka","5AHELS","24.10.2017",2,1,1,0,0,0),(null,"Wagner","2AHELS","23.10.2017",2,1,1,0,0,0),(null,"Wagner","1AHELS","25.10.2017",5,1,1,0,0,0);"`;
+  con.query(sql, function (err, result) {
+    if (err) throw err;
+    console.log("Values inserted");
+  });
+var sql = "select * from trennwandtable";
+  con.query(sql, function (err, result, fields) {
+    if (err) throw err;
+    console.log(result);
+  });
+});
