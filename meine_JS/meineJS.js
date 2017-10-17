98133f3ed0a0f0d453959f84662739e6df7166bc
leliwuz <leon.stempfer@htl-braunau.at>
2017-10-17 13:06:22 +0200

    Mein JavaScript

---
+++ meine_JS/meineJS.js
@@ -0,0 +1,18 @@
+function getdate(){
+	var date = new Date();
+//use the constructor to create by milliseconds
+	var today = new Date(date.getTime());
+	today=today.toString();
+	today=today.substring(0,15);
+ document.getElementById("today").innerHTML = today;
+ 
+ var tomorrow = new Date(date.getTime() + 24 * 60 * 60 * 1000);
+	tomorrow=tomorrow.toString();
+	tomorrow=tomorrow.substring(0,15);
+ document.getElementById("tomorrow").innerHTML = tomorrow;
+ 
+ var Ütomorrow = new Date(date.getTime() + 2 * 24 * 60 * 60 * 1000);
+	Ütomorrow=Ütomorrow.toString();
+	Ütomorrow=Ütomorrow.substring(0,15);
+ document.getElementById("Ütomorrow").innerHTML = Ütomorrow;
+}
