08c7c7481f4ab730ec6676b135cf6bd8960b8872
Wolfgang Schwendtbauer <wolfgang.schwendtbauer@gmail.com>
2017-10-20 14:25:45 +0200

    Changed "Date" datatype in Database from String to DATE

--- justfortesting/inittable.js
+++ justfortesting/inittable.js
@@ -15,7 +15,7 @@ var sql = "drop table trennwandtable;";
     if (err) throw err;
     console.log("Table dropped");
   });
-  var sql = "create table trennwandtable(id int not null auto_increment,primary key(id),teachername varchar(100),`class` varchar(10),`date` varchar(10),`lesson` int, `duration` int, trennwand1 boolean,trennwand2 boolean, trennwand3 boolean, trennwand4 boolean);";
+  var sql = "create table trennwandtable(id int not null auto_increment,primary key(id),teachername varchar(100),`class` varchar(10),`date` DATE,`lesson` int, `duration` int, trennwand1 boolean,trennwand2 boolean, trennwand3 boolean, trennwand4 boolean);";
   con.query(sql, function (err, result) {
     if (err) throw err;
     console.log("Table created");
