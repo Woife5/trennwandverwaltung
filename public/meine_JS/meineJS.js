ee46eeb5422b74bdf8218c160d59fb0a4d0becac
leliwuz <leon.stempfer@htl-braunau.at>
2017-11-21 11:33:39 +0100

    SetBeginnLesson

--- public/meine_JS/meineJS.js
+++ public/meine_JS/meineJS.js
@@ -5,20 +5,25 @@ function getdate(){
 	today=today.toString();
 	today=today.substring(0,15);
  document.getElementById("today").innerHTML = today;
- 
+
  var tomorrow = new Date(date.getTime() + 24 * 60 * 60 * 1000);
 	tomorrow=tomorrow.toString();
 	tomorrow=tomorrow.substring(0,15);
  document.getElementById("tomorrow").innerHTML = tomorrow;
- 
+
  var Ütomorrow = new Date(date.getTime() + 2 * 24 * 60 * 60 * 1000);
 	Ütomorrow=Ütomorrow.toString();
 	Ütomorrow=Ütomorrow.substring(0,15);
  document.getElementById("Ütomorrow").innerHTML = Ütomorrow;
- 
+
  var Ü2tomorrow = new Date(date.getTime() + 3 * 24 * 60 * 60 * 1000);
 	Ü2tomorrow=Ü2tomorrow.toString();
 	Ü2tomorrow=Ü2tomorrow.substring(0,15);
  document.getElementById("Ü2tomorrow").innerHTML = Ü2tomorrow;
+
+ var Ü3tomorrow = new Date(date.getTime() + 4 * 24 * 60 * 60 * 1000);
+	Ü3tomorrow=Ü3tomorrow.toString();
+	Ü3tomorrow=Ü3tomorrow.substring(0,15);
+ document.getElementById("Ü3tomorrow").innerHTML = Ü3tomorrow;
 
 }
