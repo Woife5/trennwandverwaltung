4f9ae1a3454ef1f3f623627080e4cc4ef48b8683
leliwuz <leon.stempfer@htl-braunau.at>
2017-12-05 09:55:24 +0100

    createTable.js

---
+++ public/meine_JS/createTable.js
@@ -0,0 +1,22 @@
+int SA = 10;
+
+
+var Button = '<button class="btn btn-primary" title="Trennwand-Reservierung" id="';
+var Buttonv2 = '">1</button>';
+
+document.write("<table border="
+  0 ">");
+document.write("<tr><td style='width: 100px; color: red;'>Col Head 1</td>");
+document.write("<td style='width: 100px; color: red; text-align: right;'>Col Head 2</td>");
+document.write("<td style='width: 100px; color: red; text-align: right;'>Col Head 3</td></tr>");
+
+for (var i = 0; i < 10; i++) {
+  document.write("<tr><td style='width: 100px;'>Number " + i + " is:</td>");
+  myArray[i] = myArray[i].toFixed(3);
+
+  for (var j = 0; j < 5; j++) {
+    document.write("<td style='width: 100px; text-align: right;'>" + Button + eval('i + SA * j') + Buttonv2 + "</td>");
+
+  }
+}
+document.write("</table>");
