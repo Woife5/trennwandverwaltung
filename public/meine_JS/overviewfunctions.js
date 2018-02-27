02324a03c33c8fc269457689e059fc54464ce7f2
LeonStempfer <leon.stempfer@outlook.com>
2018-02-27 10:51:45 +0100

    im Overview Stundenzeiten hinzugefügt

--- public/meine_JS/overviewfunctions.js
+++ public/meine_JS/overviewfunctions.js
@@ -12,12 +12,13 @@ function activeTab(){
 
 function generateOverview(){
   let lessonNames = ['Erste', 'Zweite', 'Dritte', 'Vierte', 'Fünfte','Sechste','Siebte','Achte','Neunte','Zehnte']
+  let stundenzeiten =[' 07:50-08:40',' 08:45-09:35',' 09:40-10:30',' 10:45-11:35',' 11:40-12:30',' 12:35-13:25',' 13:25-14:15',' 14:20-15:10',' 15:15-16:05',' 16:05-16:55']
   let generator = ''
   generator += '<h1 class="'+getColor()+'-text center-align">Übersicht über heute</h1>'
   generator += '<ul class="collapsible popout" data-collapsible="accordion">'
   for (let i = 0; i < 10; i++) {
     generator += '<li>'
-    generator += '<div class="collapsible-header"><i class="material-icons">access_time</i>'+ lessonNames[i] +' Stunde</div>'
+    generator += '<div class="collapsible-header"><i class="material-icons">access_time</i>'+ lessonNames[i] +' Stunde' +'<h6 class="right-align">'+ stundenzeiten[i]+'</h6>' + '</div>'
     generator += '<div class="collapsible-body" id="'+i+'"></div>'
     generator += '</li>'
     getOverviewContent(i)
